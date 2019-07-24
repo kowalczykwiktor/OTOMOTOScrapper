69761c7b8100f8ff75c8dc034a6b7e59de99f030
Wiktor Kowalczyk <kowalczyk.wiktor@gmail.com>
2019-07-24 09:03:40 +0200

    parametersFromPageSource

--- ScrappData.py
+++ ScrappData.py
@@ -19,3 +19,11 @@ client = MongoClient(
 db = client.otomoto
 offers = db.offers
 print(client.list_database_names())
+
+url =
+link_items_as_href = ['Oferta od', 'Kategoria', 'Marka pojazdu', 'Model pojazdu', 'Rodzaj paliwa', 'Napęd', 'Typ', 'Kolor',
+                      'Metalik', 'Perłowy', 'VAT marża', 'Kraj pochodzenia', 'Pierwszy właściciel', 'Serwisowany w ASO', 'Stan',
+                      'Wersja', 'Skrzynia biegów', 'Bezwypadkowy', 'Akryl (niemetalizowany)', 'Faktura VAT', 'Zarejestrowany w Polsce',
+                      'Używane', 'Filtr cząstek stałych', 'Kod Silnika', 'Możliwość finansowania', 'Leasing', 'Uszkodzony', 'Tuning',
+                      'Matowy', 'Homologacja ciężarowa', 'Zarejestrowany jako zabytek', 'Kierownica po prawej (Anglik)']
+convert_to_int = ['Liczba drzwi', 'Liczba miejsc', 'Rok produkcji']
