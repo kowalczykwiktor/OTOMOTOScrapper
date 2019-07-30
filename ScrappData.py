7d6b865794a6237c8b6b947e90b40823556f666b
Wiktor Kowalczyk <kowalczyk.wiktor@gmail.com>
2019-07-30 23:06:15 +0200

    addNotifications

--- ScrappData.py
+++ ScrappData.py
@@ -6,6 +6,7 @@
 # pip install -U time
 # pip install -U bs4
 # pip install -U pymongo
+# pip install -U dnspython
 
 import requests
 import time
@@ -17,6 +18,7 @@ client = MongoClient(
 db = client.otomoto
 offers = db.offers
 print(client.list_database_names())
+print('/////////////////////////////////////////////////..::Connection Successfully::..\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\')
 url = 'https://www.otomoto.pl/osobowe/?search%5Border%5D=created_at_first%3Adesc&search%5Bbrand_program_id%5D%5B0%5D=&search%5Bcountry%5D=&page='
 link_items_as_href = ['Oferta od', 'Kategoria', 'Marka pojazdu', 'Model pojazdu', 'Rodzaj paliwa', 'Napęd', 'Typ', 'Kolor',
                       'Metalik', 'Perłowy', 'VAT marża', 'Kraj pochodzenia', 'Pierwszy właściciel', 'Serwisowany w ASO', 'Stan',
@@ -116,4 +118,4 @@ for page in range(1, pages):
         count += 1
         print(count)
         time.sleep(0.5)
-print('Scrapping ended')
+print('////////////////////////////////////////////////..::Scrapping ended::..\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\')
